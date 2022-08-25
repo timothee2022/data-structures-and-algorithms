b7ac3ef66ad210acc6ed97f5d9f5eec9bda6f6e1
Timothee Odushina <hsp20tim@gmail.com>
2022-08-25 17:21:30 -0600

    completed challenge

--- javascript/code-challenges/challenges-07.test.js
+++ javascript/code-challenges/challenges-07.test.js
@@ -82,7 +82,7 @@ For example, wordsToCharList('gregor') returns ['g','r','e','g','o','r'].
 ------------------------------------------------------------------------------------------------ */
 
 const wordsToCharList = (arr) => {
-  // Solution code here...
+  let newArr = 
 };
 
 
